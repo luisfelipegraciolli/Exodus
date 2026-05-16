6b451c3e35f25e5f4858af4bf4efab835b1dd59b
Luis Felipe Graciolli <132220222+luisfelipegraciolli@users.noreply.github.com>
2026-05-16 16:38:41 -0300

    Adds ball and paddle (#3)
    * feat: Adds ball
    
    * feat: Adds paddle
    
    * feat: Adds movement and collision to the paddle
    
    * feat: Adds movement and initial velocity to the ball
    
    * feat: Adds bounce to the ball when colliding
    
    * feat: Adds celling to the main game

---
+++ Scripts/ball.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+@export var velocity: Vector2 = Vector2(250, 250)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction: Vector2 = velocity * delta
+	var colision_info: KinematicCollision2D = move_and_collide(direction) 
+	if colision_info: 
+		velocity = velocity.bounce(colision_info.get_normal())
